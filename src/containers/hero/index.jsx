20d7a08c513145fc1c3318c13956e2861f84bf02
Huzaifa-2609 <huzaifa.oct@gmail.com>
2024-02-16 00:59:33 +0500

    banner fixes

--- src/containers/hero/index.jsx
+++ src/containers/hero/index.jsx
@@ -11,12 +11,12 @@ export default function Hero() {
   return (
     <section aria-label="hero section" className="bg-neutral-100 background-gradient">
       <div className="container tablet:px-10 laptop:px-20 | pb-32 pt-14 | relative z-[0]">
-        <div className="grid grid-cols-1 min-[1300px]:grid-cols-[minmax(auto,_620px),_minmax(auto,_570px)] justify-between h-[620px] items-center gap-y-20">
+        <div className="grid grid-cols-1 min-[1200px]:grid-cols-[minmax(auto,_620px),_minmax(auto,_570px)] justify-between h-[620px] items-center gap-y-20">
           <motion.div
             initial={{ y: -100, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             transition={{ type: 'spring', duration: 1, delay: 0.3 }}
-            className="flex flex-col justify-between gap-y-8 gap-x-24 laptop:max-[1300px]:flex-row | max-laptop:max-w-xl max-laptop:mx-auto"
+            className="flex flex-col justify-between gap-y-8 gap-x-24 laptop:max-[1200px]:flex-row | max-laptop:max-w-xl max-laptop:mx-auto"
           >
             <div>
               <h1 className="text-800 text-neutral-900 font-bold font-['Montserrat',_sans-serif] leading-tight | max-w-xl mb-6">
@@ -30,7 +30,7 @@ export default function Hero() {
 
               <ContainedButton
                 type="button"
-                extraClasses="min-[1224px]:max-[1300px]:hidden |  px-7 py-3 text-500 font leading-tight"
+                extraClasses="min-[1224px]:max-[1200px]:hidden |  px-7 py-3 text-500 font leading-tight"
                 onClick={() => navigate('/auth/sign-in')}
               >
                 Get Started
@@ -56,14 +56,14 @@ export default function Hero() {
               <ContainedButton
                 type="button"
                 onClick={() => navigate('/auth/sign-in')}
-                extraClasses="min-[1300px]:hidden max-[1224px]:hidden | px-7 py-3 text-500 font-medium leading-tight"
+                extraClasses="min-[1200px]:hidden max-[1224px]:hidden | px-7 py-3 text-500 font-medium leading-tight"
               >
                 Get Started
               </ContainedButton>
             </div>
           </motion.div>
 
-          <img src={bannerImg} alt="banner-img" className="max-[1300px]:hidden " />
+          <img src={bannerImg} alt="banner-img" className="max-[1200px]:hidden " />
         </div>
 
         <div className="absolute bottom-40 left-6 -z-10">
