810975a22f5c758fd45b28c6b861a643c8c43605
Huzaifa-2609 <huzaifa.oct@gmail.com>
2024-07-02 01:10:04 +0500

    toast added on interceptor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React, { Suspense, useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import privateRoutes from './routes/privateRoutes';
 import publicRoutes from './routes/publicRoutes';
 import PageLoader from 'components/pageLoader';
 import { logout, updateUser } from './redux/actions/auth.js';
 import axiosInstance from 'services/axiosInstance';
+import Toast from 'shared/toast';
+
+const AxiosInterceptor = ({ children }) => {
+  const location = useLocation();
+  const toastRef = useRef(null);
+
+  useEffect(() => {
+    const errorHandler = (error) => {
+      if (!window.location.pathname.startsWith('/auth')) {
+        toastRef.current.showErrorToast(error?.response?.data?.message || error?.message);
+      }
+      return Promise.reject(error);
+    };
+
+    const interceptor = axiosInstance.interceptors.response.use((response) => response, errorHandler);
+
+    return () => {
+      axiosInstance.interceptors.response.eject(interceptor);
+    };
+  }, [window.location.pathname]);
+
+  return (
+    <>
+      <Toast ref={toastRef} />
+      {children}
+    </>
+  );
+};
 
 const App = () => {
   const dispatch = useDispatch();
@@ -14,11 +42,8 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(!!userState);
 
   axiosInstance.interceptors.response.use(
-    (response) => {
-      return response;
-    },
+    (response) => response,
     (error) => {
-      // If the response status is 401, perform logout
       if (error.response && error.response.status === 401) {
         dispatch(logout());
       }
@@ -38,7 +63,6 @@ const App = () => {
         const { user, seller } = response.data;
         dispatch(updateUser({ user, seller }));
       } catch (error) {
-        // Handle error
         console.error('Error fetching user:', error);
       } finally {
         setIsLoading(false);
@@ -51,13 +75,15 @@ const App = () => {
   return (
     <Router>
       <Suspense fallback={<PageLoader />}>
-        {!isLoading && (
-          <Routes>
-            {isAuthenticated
-              ? privateRoutes.map((route, index) => <Route key={index} exact {...route} />)
-              : publicRoutes.map((route, index) => <Route key={index} exact {...route} />)}
-          </Routes>
-        )}
+        <AxiosInterceptor>
+          {!isLoading && (
+            <Routes>
+              {isAuthenticated
+                ? privateRoutes.map((route, index) => <Route key={index} exact {...route} />)
+                : publicRoutes.map((route, index) => <Route key={index} exact {...route} />)}
+            </Routes>
+          )}
+        </AxiosInterceptor>
       </Suspense>
     </Router>
   );
